f7f46ef53b864ef048537d5cf49f8be94658fea8
Derick <Derick.Derick@rakuten.com>
2021-07-15 10:11:52 +0800

    Init SDK Project

---
+++ FirstModule/SharedModule.xcframework/ios-arm64_x86_64-simulator/SharedModule.framework/Headers/SharedModule.h
@@ -0,0 +1,18 @@
+//
+//  SharedModule.h
+//  SharedModule
+//
+//  Created by Derick, Derick | RASIA on 1/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SharedModule.
+FOUNDATION_EXPORT double SharedModuleVersionNumber;
+
+//! Project version string for SharedModule.
+FOUNDATION_EXPORT const unsigned char SharedModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedModule/PublicHeader.h>
+
+
